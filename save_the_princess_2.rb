b0e36a9f27a252d45186b64063161f320ea08b2a
Matt Rooney <matthewjrooney@gmail.com>
2016-04-14 10:40:57 -0600

    Add Game and Player classes. Initialize Game with input from HackerRank set up. Move mapping out of find_princess_location to its own method, scan_board_for(board, x) which takes the board and string you are scanning for as args.

--- save_the_princess_2.rb
+++ save_the_princess_2.rb
@@ -1,41 +1,64 @@
-#!/bin/ruby
-def next_move(n, r, c, grid)
-  player_coordinates   = [r, c]
-  princess_coordinates = find_princess_location(grid)
-  if vertical_check(player_coordinates[0], princess_coordinates[0]).nil?
-    puts horizontal_check(player_coordinates[1], princess_coordinates[1])
-  else
-    puts vertical_check(player_coordinates[0], princess_coordinates[0])
+class Player
+  attr_reader :x, :y
+
+  def initialize(y, x)
+    @x = x
+    @y = y
   end
 end
 
-def vertical_check(player_y, princess_y)
-  if player_y > princess_y
-    "UP"
-  elsif player_y < princess_y
-    "DOWN"
-  else
-    nil
-  end
-end
+class Game
+  attr_reader :board, :board_size
+  attr_accessor :mario, :princess
 
-def horizontal_check(player_x, princess_x)
-  if player_x > princess_x
-    puts "LEFT"
-  elsif player_x < princess_x
-    puts "RIGHT"
-  else
-    puts "You saved the Princess!"
+  def initialize(n, r, c, grid)
+    @board = grid
+    @board_size = n
+    @mario = Player.new(r, c)
   end
-end
 
-def find_princess_location(grid)
-  princess_coordinates = grid.map do |row|
-    if row.chars.include?("p")
-      [grid.index(row), row.chars.index("p")]
+  def find_princess_location(board)
+    coordinates_array = scan_board_for(board, 'p')
+    princess_coordinates = coordinates_array.compact.flatten
+    Player.new(princess_coordinates[0], princess_coordinates[1])
+  end
+
+  def scan_board_for(board, x)
+    board.map do |row|
+      if row.chars.include?(x)
+        [board.index(row), row.chars.index(x)]
+      end
     end
   end
-  princess_coordinates.compact.flatten
+
+  def next_move
+    @princess ||= find_princess_location(@board)
+    if vertical_check(@mario.y, @princess.y).nil?
+      puts horizontal_check(@mario.x, @princess.x)
+    else
+      puts vertical_check(@mario.y, @princess.y)
+    end
+  end
+
+  def vertical_check(player_y, princess_y)
+    if player_y > princess_y
+      "UP"
+    elsif player_y < princess_y
+      "DOWN"
+    else
+      nil
+    end
+  end
+
+  def horizontal_check(player_x, princess_x)
+    if player_x > princess_x
+      puts "LEFT"
+    elsif player_x < princess_x
+      puts "RIGHT"
+    else
+      puts "You saved the Princess!"
+    end
+  end
 end
 
 # read input
@@ -49,4 +72,5 @@ grid = Array.new(n)
   grid[i] = gets
 end
 
-next_move(n, r, c, grid)
+game = Game.new(n, r, c, grid)
+game.next_move
